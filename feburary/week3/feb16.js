f771c9d5b296bd5c55d94334432f65f08ce2d0e7
cryo5551 <96884049+cryo5551@users.noreply.github.com>
2022-03-07 19:26:41 +0530

    Add files via upload

---
+++ feburary/week3/feb16.js
@@ -0,0 +1,46 @@
+const http = require('http');
+const fs = require('fs');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const home = fs.readFileSync('index.html');
+const about = fs.readFileSync('./about.html');
+const register = fs.readFileSync('./register.html');
+const contacts = fs.readFileSync('./contact.html');
+
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+  url = req.url;
+
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+
+  if (url == '/') 
+  
+  { res.end(home); }
+
+  else if (url == '/about') 
+  
+  { res.end(about); }
+
+  else if (url == '/register') 
+  
+  { res.end(register); }
+
+  else if (url == '/contacts') 
+  {
+    res.end(contacts);
+  }
+
+  else {
+    res.statusCode = 404;
+    res.end('<h1>404 not found</h1>');
+  }
+
+
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
